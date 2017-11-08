18793a7ed17d0c4a6be9eec5f7e6c11827c163f1
codephp <718253589@qq.com>
2017-11-08 22:18:39 +0800

    新增入口

--- bin/set.php
+++ bin/set.php
@@ -3,7 +3,7 @@ define('BIN_PATH',__DIR__);
 define('APP_PATH',BIN_PATH."/..");
 require_once APP_PATH.'/vendor/autoload.php';
 
-checkfile("SERVER_PATH", APP_PATH."/vendor/tmtbe/swooledistributed/src/Server");
+checkfile("SERVER_PATH", APP_PATH."/vendor/guandeng/bestswoole/src/Server");
 checkfile("APP_PATH", APP_PATH . "/src/app");
 checkfile("LOG_PATH", APP_PATH . "/log");
 checkfile("PID_PATH", APP_PATH . "/pid");
