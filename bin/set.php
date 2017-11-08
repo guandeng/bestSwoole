08abe266741163c7a15f71c8051c48014485cdef
codephp <718253589@qq.com>
2017-11-08 23:49:52 +0800

    优化代码

--- bin/set.php
+++ bin/set.php
@@ -1,12 +1,12 @@
 <?php
 define('BIN_PATH',__DIR__);
-define('APP_PATH',BIN_PATH."/..");
+define('MY_PATH',BIN_PATH."/..");
 require_once APP_PATH.'/vendor/autoload.php';
 
-checkfile("SERVER_PATH", APP_PATH."/vendor/guandeng/bestswoole/src/Server");
-checkfile("APP_PATH", APP_PATH . "/src/app");
-checkfile("LOG_PATH", APP_PATH . "/log");
-checkfile("PID_PATH", APP_PATH . "/pid");
+checkfile("SERVER_PATH", MY_PATH."/vendor/guandeng/bestswoole/src/Server");
+checkfile("APP_PATH", MY_PATH . "/src/app");
+checkfile("LOG_PATH", BIN_PATH . "/log");
+checkfile("PID_PATH", BIN_PATH . "/pid");
 
 function checkfile($name,$path)
 {
