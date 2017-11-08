0d1d215168d3d9cf07e945c857eaf953aa34d117
codephp <718253589@qq.com>
2017-11-09 00:22:50 +0800

    脚手架支持

--- src/bin/define.php
+++ src/bin/define.php
@@ -1,7 +1,7 @@
 <?php
 define('BIN_PATH',__DIR__);
 define('MY_PATH',BIN_PATH."/..");
-require_once APP_PATH.'/vendor/autoload.php';
+require_once MY_PATH.'/vendor/autoload.php';
 
 checkfile("SERVER_PATH", MY_PATH."/vendor/guandeng/bestswoole/src/Server");
 checkfile("APP_PATH", MY_PATH . "/src/app");
